5f4fdaf05a14db184af8fa9ee53a2bcc4cf1617f
xyxf.git <122797375@qq.com>
2018-07-27 10:59:11 +0800

    更新流失结果数据，验证结果

--- Model/liushi/liushi.py
+++ Model/liushi/liushi.py
@@ -24,15 +24,15 @@ import time
 
 train_input = 'F:/to_xy.csv'
 # data = pd.read_csv(train_input, sep=',', header=0,iterator=True, chunksize=200000,encoding='gb2312')
-data = pd.read_csv(train_input, sep=',', header=0,iterator=True, chunksize=200000,nrows=100000)
+data = pd.read_csv(train_input, sep=',', header=0,iterator=True, chunksize=200000,nrows=200000)
 data = data.get_chunk(6000000)
 data = data.fillna(0)
 data.head(5)
 
 train_y = data.flag
-train_x = data.drop(['flag'], axis=1)
+train_x = data.drop(['flag','Ofr_Id','Unnamed: 0','Ofr_Id_last','Ofr_Id_last2'], axis=1)
 
-
+x_train, x_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.2)
 
 clf = lgb.LGBMClassifier(
     boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
@@ -40,14 +40,41 @@ clf = lgb.LGBMClassifier(
     subsample=0.7, colsample_bytree=0.7, subsample_freq=1,
     learning_rate=0.05, min_child_weight=50, random_state=2018, n_jobs=-1
 )
-clf.fit(train_x, train_y, eval_set=[(train_x, train_y)], eval_metric='auc', early_stopping_rounds=100)
+clf.fit(x_train, y_train, eval_set=[(x_train, y_train)], eval_metric='auc', early_stopping_rounds=100)
 
 y_train_pred = clf.predict(train_x)
 y_train_pred = pd.DataFrame(y_train_pred)
 train_report = metrics.classification_report(train_y, y_train_pred)
 print(train_report)
 
+
+y_train_pred = clf.predict(x_train)
+y_test_pred = clf.predict(x_test)
+train_report = metrics.classification_report(y_train, y_train_pred)
+test_report = metrics.classification_report(y_test, y_test_pred)
+print(train_report)
+print(test_report)
+
+
+# importance = clf.feature_importance()
+
+
+plt.figure(figsize=(12,6))
+lgb.plot_importance(clf, max_num_features=30)
+plt.title("Featurertances")
+plt.show()
 #   precision    recall  f1-score   support
 #           0       0.99      1.00      0.99     97720
 #           1       0.87      0.49      0.62      2280
 # avg / total       0.99      0.99      0.98    100000
+
+
+#7-27训练和验证
+#              precision    recall  f1-score   support
+#           0       0.99      1.00      0.99    156356
+#           1       0.86      0.51      0.64      3644
+# avg / total       0.99      0.99      0.99    160000
+#              precision    recall  f1-score   support
+#           0       0.99      1.00      0.99     39131
+#           1       0.69      0.42      0.52       869
+# avg / total       0.98      0.98      0.98     40000
