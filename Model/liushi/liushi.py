5c116715d737a5a793784a20ad2019873985fa8d
xyxf.git <122797375@qq.com>
2018-07-30 17:22:05 +0800

    模型维度优化

--- Model/liushi/liushi.py
+++ Model/liushi/liushi.py
@@ -1,33 +1,334 @@
 '''2018-5-9资采 不限量迁转 融合转融合模型'''
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.externals import joblib
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-import xgboost as xgb
-from xgboost import plot_importance
-from matplotlib import pyplot as plt
-from sklearn.model_selection import GridSearchCV
-from sklearn.externals import joblib
-import os
-from sqlalchemy import types, create_engine
-import random as rd
-import numpy as np
-from imblearn.over_sampling import SMOTE
-from sklearn.ensemble import RandomForestClassifier
 import lightgbm as lgb
-import time
+import pandas as pd
+# import xgboost as xgb
+from matplotlib import pyplot as plt
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+# columns = ['Prd_Inst_Id',
+#  'balance',
+#  'Bil_1X_Dur',
+#  'Bil_3G_Dur',
+#  'charge',
+#  'cwqf',
+#  'F1X_flux',
+#  'F3G_flux',
+#  'Fav_Inv_Amt',
+#  'fkzs',
+#  'fz_flag',
+#  'hbbt',
+#  'Hday_Days',
+#  'Hday_Dur',
+#  'Hday_Flux',
+#  'Home_Dur',
+#  'Home_Flux',
+#  'Inner_Rgn_Amt',
+#  'Int_Sms_Amt',
+#  'Inter_Rgn_Amt',
+#  'Inv_Amt',
+#  'lower_charge',
+#  'O_Call_Cnt',
+#  'O_Call_Dstn',
+#  'O_Call_Dur',
+#  'O_Inet_Pp_Sms_Cnt',
+#  'O_Inner_Rgn_Cnt',
+#  'O_Inner_Rgn_Dur',
+#  'O_Inter_Rgn_Cnt',
+#  'O_Inter_Rgn_Dur',
+#  'O_Onet_Pp_Sms_Cnt',
+#  'O_Sp_Sms_Cnt',
+#  'O_Tol_Cnt',
+#  'O_Tol_Dstn',
+#  'O_Tol_Dur',
+#  'Off_Dur',
+#  'Off_Flux',
+#  'Office_Dur',
+#  'Office_Flux',
+#  'Ofr_Id',
+#  'Sp_Sms_Amt',
+#  'On_Dur',
+#  'On_Flux',
+#  'Owe_Amt',
+#  'Pp_Sms_Amt',
+#  'Prom_Amt',
+#  'Recv_Rate',
+#  'T_Call_Cnt',
+#  'T_Call_Dstn',
+#  'T_Call_Dur',
+#  'T_Inet_Pp_Sms_Cnt',
+#  'T_Onet_Pp_Sms_Cnt',
+#  'T_Sp_Sms_Cnt',
+#  'Tdd_Bil_Dur',
+#  'Tdd_Flux',
+#  'Term_Mob_Price',
+#  'thsc',
+#  'Total_1X_Cnt',
+#  'Total_3G_Cnt',
+#  'Total_Flux',
+#  'Total_Tdd_Cnt',
+#  'Wday_Days',
+#  'Wday_Dur',
+#  'Wday_Flux',
+#  'ywqf',
+#  'Cer_Send_Rate',
+#  'Cer_Recv_Rate',
+#  'pro_Inv_Amt',
+#  'pro_o_dur',
+#  'pro_i_dur',
+#  'pro_cdma_nbt',
+#  'pro_flux',
+#  'yhfl',
+#  'Channel_Type_Name',
+#  'Channel_Type_Name_Lvl1',
+#  'hhmd',
+#  'Card_Type',
+#  'Std_Merge_Prom_Type_Id',
+#  'Term_Type_Id',
+#  'Accs_Grade',
+#  'black_flag',
+#  'Exp_Billing_Cycle_Id',
+#  'Gender_Id',
+#  'Innet_Billing_Cycle_Id',
+#  'Latn_Id',
+#  'Std_Prd_Inst_Stat_Id',
+#  'Strategy_Segment_Id',
+#  'zhrh',
+#  'Cde_Merge_Prom_Name_n',
+#  'Age_subsection',
+#  'balance_last',
+#  'Bil_1X_Dur_last',
+#  'Bil_3G_Dur_last',
+#  'charge_last',
+#  'cwqf_last',
+#  'F1X_flux_last',
+#  'F3G_flux_last',
+#  'Fav_Inv_Amt_last',
+#  'fkzs_last',
+#  'fz_flag_last',
+#  'hbbt_last',
+#  'Hday_Days_last',
+#  'Hday_Dur_last',
+#  'Hday_Flux_last',
+#  'Home_Dur_last',
+#  'Home_Flux_last',
+#  'Inner_Rgn_Amt_last',
+#  'Int_Sms_Amt_last',
+#  'Inter_Rgn_Amt_last',
+#  'Inv_Amt_last',
+#  'lower_charge_last',
+#  'O_Call_Cnt_last',
+#  'O_Call_Dstn_last',
+#  'O_Call_Dur_last',
+#  'O_Inet_Pp_Sms_Cnt_last',
+#  'O_Inner_Rgn_Cnt_last',
+#  'O_Inner_Rgn_Dur_last',
+#  'O_Inter_Rgn_Cnt_last',
+#  'O_Inter_Rgn_Dur_last',
+#  'O_Onet_Pp_Sms_Cnt_last',
+#  'O_Sp_Sms_Cnt_last',
+#  'O_Tol_Cnt_last',
+#  'O_Tol_Dstn_last',
+#  'O_Tol_Dur_last',
+#  'Off_Dur_last',
+#  'Off_Flux_last',
+#  'Office_Dur_last',
+#  'Office_Flux_last',
+#  'Ofr_Id_last',
+#  'Sp_Sms_Amt_last',
+#  'On_Dur_last',
+#  'On_Flux_last',
+#  'Owe_Amt_last',
+#  'Pp_Sms_Amt_last',
+#  'Prom_Amt_last',
+#  'Recv_Rate_last',
+#  'T_Call_Cnt_last',
+#  'T_Call_Dstn_last',
+#  'T_Call_Dur_last',
+#  'T_Inet_Pp_Sms_Cnt_last',
+#  'T_Onet_Pp_Sms_Cnt_last',
+#  'T_Sp_Sms_Cnt_last',
+#  'Tdd_Bil_Dur_last',
+#  'Tdd_Flux_last',
+#  'Term_Mob_Price_last',
+#  'thsc_last',
+#  'Total_1X_Cnt_last',
+#  'Total_3G_Cnt_last',
+#  'Total_Flux_last',
+#  'Total_Tdd_Cnt_last',
+#  'Wday_Days_last',
+#  'Wday_Dur_last',
+#  'Wday_Flux_last',
+#  'ywqf_last',
+#  'Cer_Send_Rate_last',
+#  'Cer_Recv_Rate_last',
+#  'pro_Inv_Amt_last',
+#  'pro_o_dur_last',
+#  'pro_i_dur_last',
+#  'pro_cdma_nbt_last',
+#  'pro_flux_last',
+#  'yhfl_last',
+#  'Channel_Type_Name_last',
+#  'Channel_Type_Name_Lvl1_last',
+#  'hhmd_last',
+#  'Card_Type_last',
+#  'Std_Merge_Prom_Type_Id_last',
+#  'Term_Type_Id_last',
+#  'Accs_Grade_last',
+#  'black_flag_last',
+#  'Exp_Billing_Cycle_Id_last',
+#  'Gender_Id_last',
+#  'Innet_Billing_Cycle_Id_last',
+#  'Latn_Id_last',
+#  'Std_Prd_Inst_Stat_Id_last',
+#  'Strategy_Segment_Id_last',
+#  'zhrh_last',
+#  'Cde_Merge_Prom_Name_n_last',
+#  'Age_subsection_last',
+#  'balance_last2',
+#  'Bil_1X_Dur_last2',
+#  'Bil_3G_Dur_last2',
+#  'charge_last2',
+#  'cwqf_last2',
+#  'F1X_flux_last2',
+#  'F3G_flux_last2',
+#  'Fav_Inv_Amt_last2',
+#  'fkzs_last2',
+#  'fz_flag_last2',
+#  'hbbt_last2',
+#  'Hday_Days_last2',
+#  'Hday_Dur_last2',
+#  'Hday_Flux_last2',
+#  'Home_Dur_last2',
+#  'Home_Flux_last2',
+#  'Inner_Rgn_Amt_last2',
+#  'Int_Sms_Amt_last2',
+#  'Inter_Rgn_Amt_last2',
+#  'Inv_Amt_last2',
+#  'lower_charge_last2',
+#  'O_Call_Cnt_last2',
+#  'O_Call_Dstn_last2',
+#  'O_Call_Dur_last2',
+#  'O_Inet_Pp_Sms_Cnt_last2',
+#  'O_Inner_Rgn_Cnt_last2',
+#  'O_Inner_Rgn_Dur_last2',
+#  'O_Inter_Rgn_Cnt_last2',
+#  'O_Inter_Rgn_Dur_last2',
+#  'O_Onet_Pp_Sms_Cnt_last2',
+#  'O_Sp_Sms_Cnt_last2',
+#  'O_Tol_Cnt_last2',
+#  'O_Tol_Dstn_last2',
+#  'O_Tol_Dur_last2',
+#  'Off_Dur_last2',
+#  'Off_Flux_last2',
+#  'Office_Dur_last2',
+#  'Office_Flux_last2',
+#  'Ofr_Id_last2',
+#  'Sp_Sms_Amt_last2',
+#  'On_Dur_last2',
+#  'On_Flux_last2',
+#  'Owe_Amt_last2',
+#  'Pp_Sms_Amt_last2',
+#  'Prom_Amt_last2',
+#  'Recv_Rate_last2',
+#  'T_Call_Cnt_last2',
+#  'T_Call_Dstn_last2',
+#  'T_Call_Dur_last2',
+#  'T_Inet_Pp_Sms_Cnt_last2',
+#  'T_Onet_Pp_Sms_Cnt_last2',
+#  'T_Sp_Sms_Cnt_last2',
+#  'Tdd_Bil_Dur_last2',
+#  'Tdd_Flux_last2',
+#  'Term_Mob_Price_last2',
+#  'thsc_last2',
+#  'Total_1X_Cnt_last2',
+#  'Total_3G_Cnt_last2',
+#  'Total_Flux_last2',
+#  'Total_Tdd_Cnt_last2',
+#  'Wday_Days_last2',
+#  'Wday_Dur_last2',
+#  'Wday_Flux_last2',
+#  'ywqf_last2',
+#  'Cer_Send_Rate_last2',
+#  'Cer_Recv_Rate_last2',
+#  'pro_Inv_Amt_last2',
+#  'pro_o_dur_last2',
+#  'pro_i_dur_last2',
+#  'pro_cdma_nbt_last2',
+#  'pro_flux_last2',
+#  'yhfl_last2',
+#  'Channel_Type_Name_last2',
+#  'Channel_Type_Name_Lvl1_last2',
+#  'hhmd_last2',
+#  'Card_Type_last2',
+#  'Std_Merge_Prom_Type_Id_last2',
+#  'Term_Type_Id_last2',
+#  'Accs_Grade_last2',
+#  'black_flag_last2',
+#  'Exp_Billing_Cycle_Id_last2',
+#  'Gender_Id_last2',
+#  'Innet_Billing_Cycle_Id_last2',
+#  'Latn_Id_last2',
+#  'Std_Prd_Inst_Stat_Id_last2',
+#  'Strategy_Segment_Id_last2',
+#  'zhrh_last2',
+#  'Cde_Merge_Prom_Name_n_last2',
+#  'Age_subsection_last2',
+#  'flag']
 
 train_input = 'F:/cdmals_543f_6l_100.txt'
 # data = pd.read_csv(train_input, sep=',', header=0,iterator=True, chunksize=200000,encoding='gb2312')
-data = pd.read_csv(train_input, sep='	', header=0,iterator=True, chunksize=200000,nrows=200000)
+data = pd.read_csv(train_input, sep='	', header=0, iterator=True, chunksize=200000, nrows=200000)
 data = data.get_chunk(6000000)
-data = data.fillna(0)
-data.head(5)
+
+
+def Fix_Missing(Enter_Date, Model=False):
+    '''MODEL等于Falst表示自动模式，自动缺失补0；等于True表示智能模式补0.1或其他(功能待完成)'''
+    if Model == False:
+        Enter_Date = Enter_Date.fillna(0)
+        Enter_Date.replace('?', 0, inplace=True)
+    return Enter_Date
+
+
+train = Fix_Missing(data)
+
+obname = list(train.select_dtypes(include=["object"]).columns)
+train_columns = list(train.columns)
+for i in obname:
+    pd.to_numeric(data[i], errors='coerce').fillna(0)    #ignore  coerce
+
+data.groupby()
+
+for col in obname:
+    train[col] = train[col].astype(np.float)
+
+A = pd.DataFrame(train['Accs_Grade'].value_counts())
+
+A = pd.DataFrame(train.groupby('Accs_Grade').size().sort_values(ascending = False),columns=['cnt'])
+S = 0
+a = list()
+for i in range(len(A)):
+    print(i)
+    Col_Sum = A.cnt.sum(axis=0)
+    if S / Col_Sum < 0.8:
+        S = A.iloc[i,0] + S
+        a.append(A.index[i])
+    else:
+        break
+print(a)
+
+train[(train.iloc[:, 161] == 0) 
+if train['Accs_Grade'] == a[0]:
+    # train['new_{}'].format(col)
+    train['Accs_Grade_New'] = 1
+
+if train['Accs_Grade'] == a[1]:
+    # train['new_{}'].format(col)
+    train['New_Accs_Grade'] = 2
+
+
+
 
 train_y = data.flag
 train_x = data.drop(['flag','Ofr_Id','Unnamed: 0','Ofr_Id_last','Ofr_Id_last2'], axis=1)
