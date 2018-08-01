ec5d6a40eba957b70ffedfb379237e486f32f875
xyxf <122797375@qq.com>
2018-08-01 17:11:28 +0800

    维度处理优化

--- Model/liushi/liushi.py
+++ Model/liushi/liushi.py
@@ -303,20 +303,38 @@ def Fix_Missing(Enter_Date, Model=False):
     return Enter_Date
 
 
+def Data_Var_Convert(input_data, label):
+    '''删除唯一值变量，自动筛选分类变量,80%单一变量值数据进行哑变量处理'''
+    new_train = input_data.drop([label], axis=1)
+    for i in train.columns:
+        Category_Count = pd.DataFrame(train.groupby(i).size(), columns=['cnt'])  # 列分类统计
+        if len(Category_Count) == 1:
+            new_train = new_train.drop([i], axis=1)  # 删除唯一值
+        elif (len(Category_Count) < 30) & (len(Category_Count) > 1):
+            Category_Count_Top1 = Category_Count.loc[0]
+            Category_Count_sum = Category_Count.cnt.sum(axis=0)
+            Top1_Bit = Category_Count_Top1 / Category_Count_sum
+            if Top1_Bit.values >= 0.95:
+                new_train = new_train.drop([i], axis=1)  # 一个维度值占比较大剔除
+            elif (Top1_Bit.values >= 0.8) & (
+                        Top1_Bit.values < 0.95):
+                new_train.loc[new_train[i] != 0, i] = 1  # 大于0.8转化为哑变量
+                categorical_feature.append(i)  # 假定分类变量
+            else:
+                categorical_feature.append(i)  # 假定分类变量
+    print('原始维度{}个,剔除后还剩下{}个'.format(input_data.shape[1], new_train.shape[1]))
+    print('自动判断分类维度共计：{}个'.format(len(categorical_feature)))
+    return categorical_feature, new_train
+
+
 #--------------------------------------------主程序区------------------------------------------------------#
 train = chunk_read_data(train_path, chunk_size, data_cnt)
 train = Fix_Missing(train)
 
-new_train = train
-for i in train.columns:
-    Category_Count = pd.DataFrame(train.groupby(i).size(), columns=['cnt'])  # 列分类统计
-    if len(Category_Count) == 1:
-        new_train = new_train.drop([i], axis=1)
-    # print('原始维度{}个,剔除后还剩下{}个'.format(train.shape[1],new_train.shape[1]) )
-    elif (len(Category_Count) < 30) & (len(Category_Count) > 1):
-        categorical_feature.append(i)
-    if Category_Count.loc[0] / Category_Count.cnt.sum(axis=0) > 0.9:
-        train[i] = train[i][train[i] != 0].replace(0)
+label = 'flag'
+cat_feature,train = Data_Var_Convert(train,label)
+
+
 
 
 
